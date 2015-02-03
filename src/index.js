e7b51a640d760eb2144f8460e9aeef43f9a69ee1
Adam K Dean <adam@amberfish.co.uk>
2015-02-03 20:06:30 +0000

    Added test blog functionality and example posts

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-var restify = require('restify');
+var _ = require('lodash'),
+    restify = require('restify'),
+    posts = require('./posts');
 
 var server = restify.createServer({
     name: 'blog-api',
@@ -15,8 +17,19 @@ server.get('/', function (req, res, next) {
 });
 
 server.get('/posts', function (req, res, next) {
-    res.send({ test: true });
+    res.send(posts);
     return next();
+});
+
+server.get('/posts/:slug', function(req, res, next) {
+    if (req.params.slug && _.contains(posts, req.params.slug)) {
+        res.send(posts[req.params.slug]);
+    } else {
+        res.send({
+            status: 404,
+            error: "blog post not found"
+        });
+    }
 });
 
 server.listen(process.env.PORT, function () {
