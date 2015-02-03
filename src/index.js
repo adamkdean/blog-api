ddbe2f0a7ba721508c23eb1c8f032f8283ffb61b
Adam K Dean <adam@amberfish.co.uk>
2015-02-03 19:59:24 +0000

    Added env port

--- src/index.js
+++ src/index.js
@@ -9,11 +9,16 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.get('/posts/', function (req, res, next) {
+server.get('/', function (req, res, next) {
+    res.send([]);
+    return next();
+});
+
+server.get('/posts', function (req, res, next) {
     res.send({ test: true });
     return next();
 });
 
-server.listen(8080, function () {
+server.listen(process.env.PORT, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
